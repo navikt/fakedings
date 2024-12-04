31f495304474346c435b4fd9cb426f08ec120dc0
Trong Huu Nguyen <trong.huu.nguyen@nav.no>
2024-12-04 08:44:51 +0100

    Revert "chore(deps): bump jvm from 2.0.21 to 2.1.0 in the gradle group (#18)"
    This reverts commit 1d057a694ea5f65041ee025d954af49c77781630.
    This change is currently incompatible with Kotlinter due to mismatch in
    bundled compilers.
    
    See #19.

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ val mainClassKt = "fakedings.ApplicationKt"
 
 plugins {
     application
-    kotlin("jvm") version "2.1.0"
+    kotlin("jvm") version "2.0.21"
     id("org.jmailen.kotlinter") version "4.5.0"
     id("com.github.johnrengelman.shadow") version "8.1.1"
     id("com.github.ben-manes.versions") version "0.51.0"
