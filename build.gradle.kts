172529cd327c0376c6053352661ff5978e8e9d0c
Trong Huu Nguyen <trong.huu.nguyen@nav.no>
2026-08-21 14:55:32 +0200

    build(deps): bump dependencies and gradle wrapper

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,8 +6,8 @@ val mainClassKt = "fakedings.ApplicationKt"
 plugins {
     application
     kotlin("jvm") version "2.4.10"
-    id("org.jmailen.kotlinter") version "5.6.0"
-    id("com.github.ben-manes.versions") version "0.54.0"
+    id("org.jmailen.kotlinter") version "5.7.0"
+    id("com.github.ben-manes.versions") version "0.61.0"
     id("se.patrikerdes.use-latest-versions") version "0.2.19"
 }
 
@@ -29,11 +29,11 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib"))
     implementation(kotlin("reflect"))
-    implementation("no.nav.security:mock-oauth2-server:5.0.2") {
+    implementation("no.nav.security:mock-oauth2-server:6.0.2") {
         exclude(group = "ch.qos.logback", module = "logback-classic")
         exclude(group = "io.github.microutils", module = "kotlin-logging")
     }
-    implementation("ch.qos.logback:logback-classic:1.5.38")
+    implementation("ch.qos.logback:logback-classic:1.6.3")
     implementation("io.github.microutils:kotlin-logging-jvm:3.0.5")
     implementation("net.logstash.logback:logstash-logback-encoder:9.0")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5:2.4.10")
